e50fc1740d4ed96563446d0e1ecea01f0598cce7
Camiloasc1 <camiloasc1@gmail.com>
2015-12-12 21:25:54 -0500

    Flippers working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject leftFlipper;
+    [SerializeField]
+    private GameObject rightFlipper;
+    [SerializeField]
+    private float speed = 1.0f;
+    private Vector3 leftRot;
+    private Vector3 rightRot;
+    private Vector3 leftTarget;
+    private Vector3 rightTarget;
+    private float tLeft = 0.0f;
+    private float tRight = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        leftRot = leftFlipper.transform.localEulerAngles;
+        rightRot = rightFlipper.transform.localEulerAngles;
+        leftTarget = leftFlipper.transform.localEulerAngles;
+        leftTarget.y = 0.0f;
+        rightTarget = rightFlipper.transform.localEulerAngles;
+        rightTarget.y = 360.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tLeft += Time.deltaTime * speed * (Input.GetKey(KeyCode.C) ? 1 : -1);
+        tLeft = Mathf.Clamp01(tLeft);
+        tRight += Time.deltaTime * speed * (Input.GetKey(KeyCode.M) ? 1 : -1);
+        tRight = Mathf.Clamp01(tRight);
+        leftFlipper.transform.localEulerAngles = Vector3.Slerp(leftRot, leftTarget, tLeft);
+        rightFlipper.transform.localEulerAngles = Vector3.Slerp(rightRot, rightTarget, tRight);
+    }
+}
